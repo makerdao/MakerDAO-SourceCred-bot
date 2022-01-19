09d527b1b4c8f5983ba8cb57bda3b5d933e74212
Hernando Guzman <hernando.guzman93@gmail.com>
2022-01-19 18:37:27 -0300

    Add command to transfer grain

---
+++ src/commands/burnGrain.ts
@@ -0,0 +1,108 @@
+import { SlashCommandBuilder } from '@discordjs/builders'
+import {
+  CommandInteraction,
+  MessageAttachment,
+  MessageActionRow,
+  MessageButton,
+  MessageComponentInteraction,
+} from 'discord.js'
+import { readFileSync, statSync } from 'fs'
+
+import { formatGrainBurnList } from '../utils/helpers'
+import { getLedgerManager, burnGrain } from '../utils/sourcecred'
+
+const SOURCECRED_ADMINS = process.env.SOURCECRED_ADMINS?.split(', ') || []
+
+// const weiToEther = (wei: string): number =>
+//   +parseFloat(formatEther(wei)).toFixed(2)
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('sc-burn-grain')
+    .setDescription(
+      'Admin only command: Burns users grain by transferring it to the Dai Redemptions account'
+    )
+    .addStringOption((option) =>
+      option
+        .setName('branch-name')
+        .setDescription('GitHub branch name created on the /sc-onboard command')
+        .setRequired(true)
+    ),
+  async execute(interaction: CommandInteraction) {
+    try {
+      if (!SOURCECRED_ADMINS.includes(interaction.user.id)) {
+        await interaction.reply({
+          content: 'Error: admin-only command',
+          ephemeral: true,
+        })
+        return
+      }
+      await interaction.deferReply({ ephemeral: true })
+
+      const row = new MessageActionRow().addComponents(
+        new MessageButton()
+          .setCustomId('confirm')
+          .setLabel('Yes')
+          .setStyle('SUCCESS'),
+        new MessageButton()
+          .setCustomId('reject')
+          .setLabel('No')
+          .setStyle('DANGER')
+      )
+
+      const paymentsBuffer = readFileSync('./payments.txt')
+
+      await interaction.editReply({
+        content: `Found this distribution file from ${
+          statSync('./payments.txt').mtime
+        }. Are these the amounts that should be burned?`,
+        files: [new MessageAttachment(paymentsBuffer, 'payments.txt')],
+        components: [row],
+      })
+
+      if (!interaction.channel)
+        throw 'There was an error fetching the Discord channel'
+      const filter = (i: MessageComponentInteraction) =>
+        (i.customId === 'confirm' || i.customId === 'reject') &&
+        i.user.id === interaction.user.id
+      const collector = interaction.channel.createMessageComponentCollector({
+        filter,
+        time: 900000,
+        max: 1,
+      })
+      collector.on('collect', async (i) => {
+        await i.update({ components: [] })
+
+        if (i.customId === 'reject') {
+          await i.followUp({
+            content:
+              'Command canceled by the user. If the distribution file is not up to date, try running the `/sc-payments-csv` command again to generate a new file based on the latest grain distribution.',
+            ephemeral: true,
+          })
+        } else {
+          await i.followUp({
+            content: 'Transferring grain for users, please wait...',
+            ephemeral: true,
+          })
+
+          const branchName = interaction.options.getString('branch-name', true)
+          const ledgerManager = await getLedgerManager(branchName)
+          const grainBurnList = formatGrainBurnList(paymentsBuffer)
+          await burnGrain(ledgerManager, grainBurnList)
+
+          await i.followUp({
+            content: `Grain succesfully transferred to the Dai Redemptions account for users on branch \`${branchName}\``,
+            ephemeral: true,
+          })
+        }
+      })
+    } catch (err) {
+      console.log(err)
+      if (typeof err === 'string') interaction.editReply(err)
+      else
+        interaction.editReply(
+          'There was an error trying to transfer grain for users, please try again'
+        )
+    }
+  },
+}
